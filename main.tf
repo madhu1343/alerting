b38acf606e18c708fcdfdc8966cd2a258702cde3
madhu1343 <69269793+madhu1343@users.noreply.github.com>
2020-08-13 18:24:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_storage_account" "to_monitor" {
-  name                     = "examplesa20200813"
+  name                     = "testsa20200813"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
