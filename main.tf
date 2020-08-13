59f8790cbe199956019896762d22f5e9fe7b1e61
madhu1343 <69269793+madhu1343@users.noreply.github.com>
2020-08-13 19:39:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,24 +39,7 @@ resource "azurerm_monitor_metric_alert" "test" {
   scopes              = [azurerm_storage_account.to_monitor.id]
   description         = "Action will be triggered when Transactions count is greater than 50."
 
-  criteria {
-    metric_namespace = "Microsoft.Storage/storageAccounts"
-    metric_name      = "Transactions"
-    aggregation      = "Total"
-    operator         = "GreaterThan"
-    threshold        = 50
-
-    dimension {
-      name     = "ApiName"
-      operator = "Include"
-      values   = ["*"]
-    }
-    }
-
-  action {
-    action_group_id = azurerm_monitor_action_group.main.id
-  }
-
+ 
 criteria {
 metric_namespace = "Microsoft.Storage/storageAccounts"
 metric_name = "Availability"
